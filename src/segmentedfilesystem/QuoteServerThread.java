83ef0058c4a4cddc720aa088e780ba36ef21a366
holma198 <holma198@morris.umn.edu>
2017-11-21 11:52:36 -0600

    Alter the QuoteServerThread method, such that a datagram packet can be read from the server and start on a way to store each packets contents in an array to properly order the data, still needs to be tested.

--- src/segmentedfilesystem/QuoteServerThread.java
+++ src/segmentedfilesystem/QuoteServerThread.java
@@ -1,27 +1,46 @@
 package segmentedfilesystem;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.Socket;
+import java.util.Stack;
 
 
-public class QuoteServerThread {
+public class QuoteServerThread extends Thread{
     public QuoteServerThread() throws IOException {
+        this("QuoteServer");
     }
+    Stack<byte[]> packetStruct = new Stack<>();
+    int stackSize = 0;
     DatagramSocket socket = new DatagramSocket();
     BufferedReader in;
-    public QuoteServerThread(String name) throws IOException {
-        //super(name);
-        socket = new DatagramSocket(6014);
 
+
+    public QuoteServerThread(String name) throws IOException {
+            socket = new DatagramSocket(6014);
+    }
+    public boolean foundFiles = false;
+    public void run() {
         try {
-            in = new BufferedReader(new FileReader("ayyyy-LMAO.txt"));
-        } catch (FileNotFoundException e){
+
+            while (!foundFiles) {
+                byte[] buf = new byte[256];
+                DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                socket.receive(packet);
+                packetStruct.push(buf);
+                stackSize++;
+            }
+            DatagramPacket[] packetArray = new DatagramPacket[stackSize];
+            while(stackSize != 0){
+                if(packetStruct.peek()[0]%2==0){
+                    //packetArray[0] = packetStruct.pop();
+                }
+                stackSize--;
+            }
+        } catch(IOException e){
             e.printStackTrace();
         }
-
     }
+
 }
