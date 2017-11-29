2c2cb274c7df8ef8c0689d4b7eef1d7e07359323
holma198 <holma198@morris.umn.edu>
2017-11-28 18:18:16 -0600

    store each file as a single byte array and add each of these files in an arrayList.

--- src/segmentedfilesystem/QuoteServerThread.java
+++ src/segmentedfilesystem/QuoteServerThread.java
@@ -27,33 +27,58 @@ public class QuoteServerThread extends Thread{
     public void run() {
         try {
             int upperLimit = Integer.MAX_VALUE;
-            while (stackSize < upperLimit) {
-                byte[] buf = new byte[256];
-                DatagramPacket packet = new DatagramPacket(buf, buf.length);
-                socket.receive(packet);
-                String packetNumber;
-                byte[] packetNumArr = Arrays.copyOfRange(buf,2,3);
-                packetNumber = (packetNumArr[0] + "" + packetNumArr[1]);
-                int realPacketNumber = Integer.parseInt(packetNumber);
-                packetStruct.push(buf);
-                stackSize++;
-
-                if(buf[0]%4 == 3){
-                    upperLimit = realPacketNumber;
-                }
-            }
-            ArrayList<byte[]> packetArray = new ArrayList<byte[]>();
-            while(stackSize != 0){
-                if(packetStruct.peek()[0]%2==0){
-                    packetArray.add(0, packetStruct.pop());
-                } else {
+            byte[] idList = new byte[3];
+            int byteArrCounter = 0;
+            int fileID = -1;
+            ArrayList<byte[]> files = new ArrayList<>();
+            for(int i = 1; i < 4; i++) {
+                stackSize = 0;
+                while (stackSize <= upperLimit) {
+                    byte[] buf = new byte[1024];
+                    DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                    socket.receive(packet);
                     String packetNumber;
-                    byte[] packetNumArr = Arrays.copyOfRange(packetStruct.peek(),2,3);
-                    packetNumber = (packetNumArr[0] + "" + packetNumArr[1]);
-                    int realPacketNumber = Integer.parseInt(packetNumber);
-                    packetArray.add(realPacketNumber - 1, packetStruct.pop());
+                    byte[] packetNumArr = Arrays.copyOfRange(buf, 2, 3);
+                    if ((packetNumArr[1] == fileID) && fileID != -1) {
+                        packetNumber = (packetNumArr[0] + "" + packetNumArr[1]);
+                        int realPacketNumber = Integer.parseInt(packetNumber);
+                        packetStruct.push(buf);
+                        stackSize++;
+                        if (buf[0] % 4 == 3) {
+                            upperLimit = realPacketNumber;
+                        }
+                    } else {
+                        idList[byteArrCounter] = packetNumArr[1];
+                        byteArrCounter++;
+                    }
                 }
-                stackSize--;
+                ArrayList<byte[]> packetArray = new ArrayList<byte[]>();
+                int packetCount = stackSize + 1;
+                while(stackSize != 0){
+                    if(packetStruct.peek()[0]%2==0){
+                        packetArray.add(0, packetStruct.pop());
+                    } else {
+                        String packetNumber;
+                        byte[] packetNumArr = Arrays.copyOfRange(packetStruct.peek(),2,3);
+                        packetNumber = (packetNumArr[0] + "" + packetNumArr[1]);
+                        int realPacketNumber = Integer.parseInt(packetNumber);
+                        packetArray.add(realPacketNumber, packetStruct.pop());
+                    }
+                    stackSize--;
+                }
+                //make 3 array to store each files bytes
+                byte[] fullFile = new byte[packetCount*1024];
+                int incrementer = 0;
+                for(int j = 0; j < packetCount; j++){
+
+                    for(int x = 0; x < 1024; x++){
+                        fullFile[incrementer] = packetArray.get(j)[x];
+                        incrementer++;
+                    }
+
+                }
+                fileID = idList[i];
+                files.add(fullFile);
             }
         } catch(IOException e){
             e.printStackTrace();
