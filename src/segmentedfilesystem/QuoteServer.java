1c1ae10595fa96a9490e8f4cdaf723c43b46d62a
holma198 <holma198@morris.umn.edu>
2017-11-28 18:19:04 -0600

    Create an empty packet to send to the server to begin the communication with the server from the client.

--- src/segmentedfilesystem/QuoteServer.java
+++ src/segmentedfilesystem/QuoteServer.java
@@ -1,9 +1,21 @@
 package segmentedfilesystem;
-
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
 
 public class QuoteServer {
     public static void main(String[] args) throws IOException {
-        //new QuoteServerThread().start();
+        try {
+            DatagramSocket socket = new DatagramSocket(6014);
+            byte[] startbuffer = new byte[1024];
+            DatagramPacket emptyPacket = new DatagramPacket(startbuffer,1024);
+            socket.send(emptyPacket);
+        }
+        catch(SocketException e){
+            e.printStackTrace();
+        }
+        new QuoteServerThread().start();
+
     }
 }
